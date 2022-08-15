ad46d5e2176f2d17e83da8ab71d633cea6642ad4
jaeeon <wodjs12345@naver.com>
2022-08-15 21:35:39 +0900

    zxczxc

---
+++ springtest/src/main/java/com/myproject/common/logger/LoggerInterceptor.java
@@ -0,0 +1,36 @@
+package com.myproject.common.logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoggerInterceptor extends HandlerInterceptorAdapter{
+	protected Logger log = LoggerFactory.getLogger(LoggerInterceptor.class);
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		if (log.isDebugEnabled()) {
+			log.debug("======================================          START         ======================================");
+			log.debug(" Request URI \t:  " + request.getRequestURI());
+		}
+		else{
+			System.out.println("로그포제이 설정이 안먹네 Sssssssssssssssssssss시작");
+		}
+		return super.preHandle(request, response, handler);
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		if (log.isDebugEnabled()) {
+			log.debug("======================================           END          ======================================\n");
+		}
+		else{
+			System.out.println("Ssssssssssssssssssss끝");
+		}
+	}
+	
+}
